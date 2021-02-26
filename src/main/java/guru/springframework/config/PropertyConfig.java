d9cd0917618324b147f731791839376f2967b4ba
geekr <geek.rupam@gmail.com>
2021-02-26 16:10:08 -0500

    no change

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,4 +1,38 @@
 package guru.springframework.config;
 
+import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties(){
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer= new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+    @Value("${guru.username}")
+    private String userName;
+
+    @Value("${guru.password}")
+    private String password;
+
+    @Value("${guru.dburl}")
+    private String url;
+
+
+    @Bean
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource= new FakeDataSource();
+        fakeDataSource.setUserName(userName);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+
+        return fakeDataSource;
+    }
 }
